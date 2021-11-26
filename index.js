82928ff8057de4492e32f2648f4d0538fcedfffd
jm011jh <jm011jh@gmail.com>
2021-11-26 14:41:31 +0900

    SJM khan init...

---
+++ index.js
@@ -0,0 +1,249 @@
+//KHAN GOLI info==================================================
+var khangoli5W30 = {
+    name: 'KHAN GOLI 5W-30',
+    style:'FULLY SYNTHETIC GASOLINE',
+    level:'API SP-RC / ILSAC GF-6A',
+    description:'KHAN GOLI is a high performance fully synthetic engine oil, formulated from the latest additives,the cutting-edge technology(TET) and the highest performance synthetic base oil to meet the latest API SP-RC and ILSAC GF-6A specifications. It is developed to provide optimized engine oil performance, especially improved durability and fuel economy with wear protection and friction reduction. In addition, it is suitable for modern vehicles against LSPI.'
+}
+var khangoli0W20 = {
+    name: 'KHAN GOLI 0W-20',
+    style:'FULLY SYNTHETIC GASOLINE',
+    level:'API SP-RC / ILSAC GF-6A',
+    description:'KHAN GOLI is a high performance fully synthetic engine oil, formulated from the latest additives,the cutting-edge technology(TET) and the highest performance synthetic base oil to meet the latest API SP-RC and ILSAC GF-6A specifications. It is developed to provide optimized engine oil performance, especially improved durability and fuel economy with wear protection and friction reduction. In addition, it is suitable for modern vehicles against LSPI.'
+}
+var khangoli0w30 = {
+    name: 'KHAN GOLI 0W-30',
+    style:'FULLY SYNTHETIC GASOLINE',
+    level:'API SP-RC / ILSAC GF-6A',
+    description:'KHAN GOLI is a high performance fully synthetic engine oil, formulated from the latest additives,the cutting-edge technology(TET) and the highest performance synthetic base oil to meet the latest API SP-RC and ILSAC GF-6A specifications. It is developed to provide optimized engine oil performance, especially improved durability and fuel economy with wear protection and friction reduction. In addition, it is suitable for modern vehicles against LSPI.'
+}
+var khangoli10w30 = {
+    name: 'KHAN GOLI 10W-30',
+    style:'FULLY SYNTHETIC GASOLINE',
+    level:'API SP-RC / ILSAC GF-6A',
+}
+var khangoli0w16 = {
+    name: 'KHAN GOLI 0W-16',
+    style:'FULLY SYNTHETIC GASOLINE',
+    level:'API SP-RC / ILSAC GF-6B',
+}
+var khangoli5w40 = {
+    name: 'KHAN GOLI 5W-40',
+    style:'FULLY SYNTHETIC GASOLINE',
+    level:'API SP-RC',
+}
+var khangoli10w40 = {
+    name: 'KHAN GOLI 10W-40',
+    style:'FULLY SYNTHETIC GASOLINE',
+    level:'API SP-RC',
+}
+//KHAN PAO info==================================================
+var khanpao0w30 = {
+    name: 'KHAN PAO 0W-30',
+    style:'FULLY SYNTHETIC GASOLINE',
+    level:'API SN/CF ACEA C2-12, ACEA A5/B5-12 MB 229.31/229.51, VW 502.00/505.00, BMW LL-04',
+}
+var khanpao0w40 = {
+    name: 'KHAN PAO 0W-40',
+    style:'FULLY SYNTHETIC GASOLINE',
+    level:'API SN/CF ACEA C3 MB 229.31/229.51, VW 502.00/505.00, BMW LL-04',
+}
+var khanpao5w30 = {
+    name: 'KHAN PAO 5W-30',
+    style:'FULLY SYNTHETIC GASOLINE',
+    level:'API SN ACEA A3/B4 MB229.3, VW502.00 / 505.00'
+}
+var khanpao5w40 = {
+    name: 'KHAN PAO 5W-40',
+    style:'FULLY SYNTHETIC GASOLINE',
+    level:'API SN/CF ACEA C3-12, API SN BMW LL-04, MB 229.31/229.51, VW 502.00/505.01'
+}
+var khanpao0w20 = {
+    name: 'KHAN PAO 0W-20',
+    style:'FULLY SYNTHETIC GASOLINE',
+    level:'API SN/CF ACEA C3, ACEA A3/B4 MB 229.31/229.51, VW 502.00/505.01, BMW LL-04'
+}
+var khanpao5w50 = {
+    name: 'KHAN PAO 5W-50',
+    style:'FULLY SYNTHETIC GASOLINE',
+    level:'API SN/CF ACEA C3, ACEA A3/B4 MB 229.31/229.51, VW 502.00/505.01, BMW LL-04'
+}
+//KHAN G info==================================================
+var khang0w16 = {
+    name: 'KHAN G 0W-16',
+    style:'FULLY SYNTHETIC GASOLINE',
+    level:'API SN, ILSAC GF-5'
+}
+var khang0w20 = {
+    name: 'KHAN G 0W-20',
+    style:'FULLY SYNTHETIC GASOLINE',
+    level:'API SN-RC/CF, ILSAC GF-5'
+}
+var khang0w30 = {
+    name: 'KHAN G 0W-30',
+    style:'FULLY SYNTHETIC GASOLINE',
+    level:'API SN-RC/CF, ILSAC GF-5'
+}
+var khang0w40 = {
+    name: 'KHAN G',
+    style:'FULLY SYNTHETIC GASOLINE',
+    level:'API SN/CF'
+}
+var khang5w20 = {
+    name: 'KHAN G 5W-20',
+    style:'FULLY SYNTHETIC GASOLINE',
+    level:'API SN-RC/CF, ILSAC GF-5'
+}
+var khang5w30 = {
+    name: 'KHAN G 5W-30',
+    style:'FULLY SYNTHETIC GASOLINE',
+    level:'API SN/CF MB 229.3, VW502.00/505.00 GM dexos1(First Generation)'
+}
+var khang5w40 = {
+    name: 'KHAN G 5W-40',
+    style:'FULLY SYNTHETIC GASOLINE',
+    level:'API SN/CF'
+}
+var khang5w50 = {
+    name: 'KHAN G 5W-50',
+    style:'FULLY SYNTHETIC GASOLINE',
+    level:'API SN/CF'
+}
+var khang10w30 = {
+    name: 'KHAN G 10W-30',
+    style:'FULLY SYNTHETIC GASOLINE',
+    level:'API SN-RC/CF, ILSAC GF-5'
+}
+var khang10w40 = {
+    name: 'KHAN G 10W-40',
+    style:'FULLY SYNTHETIC GASOLINE',
+    level:'API SN/CF MB 229.3, VW502.00/505.00'
+}
+var khang20w50 = {
+    name: 'KHAN G 20W-50',
+    style:'FULLY SYNTHETIC GASOLINE',
+    level:'API SN/CF'
+}
+//KHAN G-SL ...waiting...==================================================
+
+//KHAN D info==================================================
+var khandc35w30 = {
+    name: 'KHAN D C3 5W-30',
+    style:'FULLY SYNTHETIC GASOLINE',
+    level:'ACEA C3, ACEA A3/B4 MB 229.31, 229.51 / VW 502.00, 505.01 /BMW LL-04'
+}
+var khandc45w30 = {
+    name: 'KHAN D C4 5W-30',
+    style:'FULLY SYNTHETIC GASOLINE',
+    level:'ACEA C4 | ACEA C2 | Renault RN0720'
+}
+var khandc45w40 = {
+    name: 'KHAN D C4 5W-40',
+    style:'FULLY SYNTHETIC GASOLINE',
+    level:'ACEA C4 | Renault RN0720'
+}
+var khandc50w20 = {
+    name: 'KHAN D C5 0W-20',
+    style:'FULLY SYNTHETIC GASOLINE',
+    level:'ACEA C5'
+}
+var khandc55w20 = {
+    name: 'KHAN D C5 5W-20',
+    style:'FULLY SYNTHETIC GASOLINE',
+    level:'ACEA C5'
+}
+//KHAN C-8 ...waiting......==================================================
+//KHAN C-7
+var khanc710w30 = {
+    name: 'KHAN C-7 10W-30',
+    style:'SYNTHETIC DIESEL',
+    level:'API CJ-4, CI-4 PLUS, CI-4, SM, ACEA E9-12 VDS-4, Mack EO-O Premium Plus, Renault RLD-3 , CES 20081, MB 228.31, Cat ECF-3, ECF-2, ECF-1-A, MAN 3575, Detroit Diesel DDC 93K218 '
+}
+var khanc710w40 = {
+    name: 'KHAN C-7 10W-40',
+    style:'SYNTHETIC DIESEL',
+    level:'API CJ-4/SN | ACEA E9 MAN 3275 | Cummins CES20081 | Caterpillar ECF-3 | DDC 93K218 | Mack EO-N Premium Plus 03'
+}
+var khanc715w40 = {
+    name: 'KHAN C-7 15W-40',
+    style:'SYNTHETIC DIESEL',
+    level:'API CJ-4, CI-4 PLUS, CI-4, SM, ACEA E9-12 VDS-4, Mack EO-O Premium Plus, Renault RLD-3 , CES 20081, MB 228.31, Cat ECF-3, ECF-2, ECF-1-A, MAN 3575, Detroit Diesel DDC 93K218'
+}
+//KHAN C-6 ...waiting......==================================================
+//KHAN C-5 ...waiting......==================================================
+//KHAN C-3 ...waiting......==================================================
+//KHAN BIKE 4T==================================================
+var khanbike4t10w40 = {
+    name: 'KHAN BIKE 4T 10W-40',
+    style:'FULLY SYNTHETIC BIKE',
+    level:'API SN JASO MB'
+}
+var khanbike4t20w50 = {
+    name: 'KHAN BIKE 4T 20W-50',
+    style:'FULLY SYNTHETIC BIKE',
+    level:'API SL JASO MA2'
+}
+var khanbike4t10w30 = {
+    name: 'KHAN BIKE 4T 10W-30',
+    style:'FULLY SYNTHETIC BIKE',
+    level:'API SL/CF JASO MA'
+}
+var khanbike4t10w40 = {
+    name: 'KHAN BIKE 4T 10W-40',
+    style:'FULLY SYNTHETIC BIKE',
+    level:'API SL/CF JASO MA'
+}
+var khanbike4t20w40 = {
+    name: 'KHAN BIKE 4T 20W-40',
+    style:'FULLY SYNTHETIC BIKE',
+    level:'API SL/CF JASO MA'
+}
+var khanbike4t20w50 = {
+    name: 'KHAN BIKE 4T 20W-50',
+    style:'FULLY SYNTHETIC BIKE',
+    level:'API SL/CF JASO MA'
+}
+//KHAN MTF GL-5==================================================
+var khanmtfgl575w90 = {
+    name: 'KHAN MTF GL-5 75W-90',
+    style:'Manual Transmission Fluid',
+    level:'API GL-5'
+}
+var khanmtfgl580w90 = {
+    name: 'KHAN MTF GL-5 80W-90',
+    style:'Manual Transmission Fluid',
+    level:'API GL-5'
+}
+var khanmtfgl585w140 = {
+    name: 'KHAN MTF GL-5 85W-140',
+    style:'Manual Transmission Fluid',
+    level:'API GL-5'
+}
+//KHAN ATF==================================================
+var khanatf3 = {
+    name: 'KHAN ATF 3',
+    style:'SYNTHETIC ATF',
+    level:'Dexron IIIH/M | Allison C-4'
+}
+var khanatfmulti = {
+    name: 'KHAN ATF MULTI',
+    style:'SYNTHETIC ATF',
+    level:'ATF MULTI'
+}
+//KHAN HYDRAULIC AW==================================================
+var khanaw32 = {
+    name: 'KHAN HYDRAULIC AW ',
+    style:'물어봐야함',
+    level:'ISO VG 32'
+}
+var khanaw46 = {
+    name: 'KHAN HYDRAULIC AW ',
+    style:'물어봐야함',
+    level:'ISO VG 46'
+}
+var khanaw68 = {
+    name: 'KHAN HYDRAULIC AW ',
+    style:'물어봐야함',
+    level:'ISO VG 68'
+}
+//KHAN HYDRAULIC HV ...waiting...==================================================
