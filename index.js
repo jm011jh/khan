5f589cb9f33afd7bff61c2e20050234153b97b2b
jm011jh <jm011jh@gmail.com>
2021-11-29 19:12:27 +0900

    SJM Done

--- index.js
+++ index.js
@@ -1,291 +1,475 @@
+// #region product information=======================================
 //KHAN GOLI info==================================================
 var khangoli01 = {
     name: '0W-16',
     style:'FULLY SYNTHETIC GASOLINE',
     level:'API SP-RC / ILSAC GF-6B',
+    logoSAE:'true',
+    logoFor:'false',
+    logo0W:'true'
 }
 var khangoli02 = {
     name: '0W-20',
     style:'FULLY SYNTHETIC GASOLINE',
     level:'API SP-RC / ILSAC GF-6A',
+    logoSAE:'true',
+    logoFor:'true',
+    logo0W:'false'
 }
 var khangoli03 = {
     name: '0W-30',
     style:'FULLY SYNTHETIC GASOLINE',
     level:'API SP-RC / ILSAC GF-6A',
+    logoSAE:'true',
+    logoFor:'true',
+    logo0W:'false'
 }
 var khangoli04 = {
     name: '5W-30',
     style:'FULLY SYNTHETIC GASOLINE',
     level:'API SP-RC / ILSAC GF-6A',
+    logoSAE:'true',
+    logoFor:'true',
+    logo0W:'false'
 }
 var khangoli05 = {
     name: '5W-40',
     style:'FULLY SYNTHETIC GASOLINE',
     level:'API SP-RC',
+    logoSAE:'true',
+    logoFor:'false',
+    logo0W:'false'
 }
 var khangoli06 = {
     name: '10W-30',
     style:'FULLY SYNTHETIC GASOLINE',
     level:'API SP-RC / ILSAC GF-6A',
+    logoSAE:'true',
+    logoFor:'true',
+    logo0W:'false'
 }
 
 var khangoli07 = {
     name: '10W-40',
     style:'FULLY SYNTHETIC GASOLINE',
     level:'API SP-RC',
+    logoSAE:'true',
+    logoFor:'false',
+    logo0W:'false'
 }
 const khanGoliProduct = [khangoli01,khangoli02,khangoli03,khangoli04,khangoli05,khangoli06,khangoli07]
-const khanGoli = $('#khanGoli')
+const khanGoli = jQuery('#khanGoli')
 //KHAN PAO info==================================================
 var khanpao01 = {
     name: '0W-30',
     style:'FULLY SYNTHETIC GASOLINE',
     level:'API SN/CF ACEA C2-12, ACEA A5/B5-12 MB 229.31/229.51, VW 502.00/505.00, BMW LL-04',
+    level:'API SP-RC',
+    logoSAE:'false',
+    logoFor:'false',
+    logo0W:'false'
 }
 var khanpao02 = {
     name: '0W-40',
     style:'FULLY SYNTHETIC GASOLINE',
     level:'API SN/CF ACEA C3 MB 229.31/229.51, VW 502.00/505.00, BMW LL-04',
+    logoSAE:'false',
+    logoFor:'false',
+    logo0W:'false'
 }
 var khanpao03 = {
     name: '5W-30',
     style:'FULLY SYNTHETIC GASOLINE',
-    level:'API SN ACEA A3/B4 MB229.3, VW502.00 / 505.00'
+    level:'API SN ACEA A3/B4 MB229.3, VW502.00 / 505.00',
+    logoSAE:'false',
+    logoFor:'false',
+    logo0W:'false'
 }
 var khanpao04 = {
     name: '5W-40',
     style:'FULLY SYNTHETIC GASOLINE',
-    level:'API SN/CF ACEA C3-12, API SN BMW LL-04, MB 229.31/229.51, VW 502.00/505.01'
+    level:'API SN/CF ACEA C3-12, API SN BMW LL-04, MB 229.31/229.51, VW 502.00/505.01',
+    logoSAE:'false',
+    logoFor:'false',
+    logo0W:'false'
 }
 var khanpao05 = {
     name: '0W-20',
     style:'FULLY SYNTHETIC GASOLINE',
-    level:'API SN/CF ACEA C3, ACEA A3/B4 MB 229.31/229.51, VW 502.00/505.01, BMW LL-04'
+    level:'API SN/CF ACEA C3, ACEA A3/B4 MB 229.31/229.51, VW 502.00/505.01, BMW LL-04',
+    logoSAE:'false',
+    logoFor:'false',
+    logo0W:'false'
 }
 var khanpao06 = {
     name: '5W-50',
     style:'FULLY SYNTHETIC GASOLINE',
-    level:'API SN/CF ACEA C3, ACEA A3/B4 MB 229.31/229.51, VW 502.00/505.01, BMW LL-04'
+    level:'API SN/CF ACEA C3, ACEA A3/B4 MB 229.31/229.51, VW 502.00/505.01, BMW LL-04',
+    logoSAE:'false',
+    logoFor:'false',
+    logo0W:'false'
 }
 const khanPaoProduct = [khanpao01,khanpao02,khanpao03,khanpao04,khanpao05,khanpao06]
-const khanPao = $('#khanPao')
+const khanPao = jQuery('#khanPao')
 //KHAN G info==================================================
 var khang01 = {
     name: '0W-16',
     style:'FULLY SYNTHETIC GASOLINE',
-    level:'API SN, ILSAC GF-5'
+    level:'API SN, ILSAC GF-5',
+    logoSAE:'false',
+    logoFor:'true',
+    logo0W:'false'
 }
 var khang02 = {
     name: '0W-20',
     style:'FULLY SYNTHETIC GASOLINE',
-    level:'API SN-RC/CF, ILSAC GF-5'
+    level:'API SN-RC/CF, ILSAC GF-5',
+    logoSAE:'false',
+    logoFor:'true',
+    logo0W:'false'
 }
 var khang03 = {
     name: '0W-30',
     style:'FULLY SYNTHETIC GASOLINE',
-    level:'API SN-RC/CF, ILSAC GF-5'
+    level:'API SN-RC/CF, ILSAC GF-5',
+    logoSAE:'false',
+    logoFor:'true',
+    logo0W:'false'
 }
 var khang04 = {
     name: '0W-40',
     style:'FULLY SYNTHETIC GASOLINE',
-    level:'API SN/CF'
+    level:'API SN/CF',
+    logoSAE:'false',
+    logoFor:'false',
+    logo0W:'false'
 }
 var khang05 = {
     name: ' 5W-20',
     style:'FULLY SYNTHETIC GASOLINE',
-    level:'API SN-RC/CF, ILSAC GF-5'
+    level:'API SN-RC/CF, ILSAC GF-5',
+    logoSAE:'false',
+    logoFor:'true',
+    logo0W:'false'
 }
 var khang06 = {
     name: '5W-30',
     style:'FULLY SYNTHETIC GASOLINE',
-    level:'API SN/CF MB 229.3, VW502.00/505.00 GM dexos1(First Generation)'
+    level:'API SN/CF MB 229.3, VW502.00/505.00 GM dexos1(First Generation)',
+    logoSAE:'false',
+    logoFor:'false',
+    logo0W:'false'
 }
 var khang07 = {
     name: '5W-40',
     style:'FULLY SYNTHETIC GASOLINE',
-    level:'API SN/CF'
+    level:'API SN/CF',
+    logoSAE:'false',
+    logoFor:'false',
+    logo0W:'false'
 }
 var khang08 = {
     name: '5W-50',
     style:'FULLY SYNTHETIC GASOLINE',
-    level:'API SN/CF'
+    level:'API SN/CF',
+    logoSAE:'false',
+    logoFor:'false',
+    logo0W:'false'
 }
 var khang09 = {
     name: '10W-30',
     style:'FULLY SYNTHETIC GASOLINE',
-    level:'API SN-RC/CF, ILSAC GF-5'
+    level:'API SN-RC/CF, ILSAC GF-5',
+    logoSAE:'false',
+    logoFor:'true',
+    logo0W:'false'
 }
 var khang10 = {
     name: '10W-40',
     style:'FULLY SYNTHETIC GASOLINE',
-    level:'API SN/CF MB 229.3, VW502.00/505.00'
+    level:'API SN/CF MB 229.3, VW502.00/505.00',
+    logoSAE:'false',
+    logoFor:'false',
+    logo0W:'false'
 }
 var khang11 = {
     name: '20W-50',
     style:'FULLY SYNTHETIC GASOLINE',
-    level:'API SN/CF'
+    level:'API SN/CF',
+    logoSAE:'false',
+    logoFor:'false',
+    logo0W:'false'
 }
-const khanGProduct = [khanpao01,khanpao02,khanpao03,khanpao04,khanpao05,khanpao06,khang07,khang08,khang09,khang10,khang11]
-const khanG = $('#khanG')
+const khanGProduct = [khang01,khang02,khang03,khang04,khang05,khang06,khang07,khang08,khang09,khang10,khang11]
+const khanG = jQuery('#khanG')
 //KHAN G-SL ...waiting...==================================================
 
 //KHAN D info==================================================
-var khandc35w30 = {
+var khand01 = {
     name: 'KHAN D C3 5W-30',
     style:'FULLY SYNTHETIC GASOLINE',
-    level:'ACEA C3, ACEA A3/B4 MB 229.31, 229.51 / VW 502.00, 505.01 /BMW LL-04'
+    level:'ACEA C3, ACEA A3/B4 MB 229.31, 229.51 / VW 502.00, 505.01 /BMW LL-04',
+    logoSAE:'false',
+    logoFor:'false',
+    logo0W:'false'
 }
-var khandc45w30 = {
+var khand02 = {
     name: 'KHAN D C4 5W-30',
     style:'FULLY SYNTHETIC GASOLINE',
-    level:'ACEA C4 | ACEA C2 | Renault RN0720'
+    level:'ACEA C4 | ACEA C2 | Renault RN0720',
+    logoSAE:'false',
+    logoFor:'false',
+    logo0W:'false'
 }
-var khandc45w40 = {
+var khand03 = {
     name: 'KHAN D C4 5W-40',
     style:'FULLY SYNTHETIC GASOLINE',
-    level:'ACEA C4 | Renault RN0720'
+    level:'ACEA C4 | Renault RN0720',
+    logoSAE:'false',
+    logoFor:'false',
+    logo0W:'false'
 }
-var khandc50w20 = {
+var khand04 = {
     name: 'KHAN D C5 0W-20',
     style:'FULLY SYNTHETIC GASOLINE',
-    level:'ACEA C5'
+    level:'ACEA C5',
+    logoSAE:'false',
+    logoFor:'false',
+    logo0W:'false'
 }
-var khandc55w20 = {
+var khand05 = {
     name: 'KHAN D C5 5W-20',
     style:'FULLY SYNTHETIC GASOLINE',
-    level:'ACEA C5'
+    level:'ACEA C5',
+    logoSAE:'false',
+    logoFor:'false',
+    logo0W:'false'
 }
+const khanDProduct = [khand01,khand02,khand03,khand04,khand05]
+const khanD = jQuery('#khanD')
 //KHAN C-8 ...waiting......==================================================
 //KHAN C-7
-var khanc710w30 = {
+var khanc01 = {
     name: 'KHAN C-7 10W-30',
     style:'SYNTHETIC DIESEL',
-    level:'API CJ-4, CI-4 PLUS, CI-4, SM, ACEA E9-12 VDS-4, Mack EO-O Premium Plus, Renault RLD-3 , CES 20081, MB 228.31, Cat ECF-3, ECF-2, ECF-1-A, MAN 3575, Detroit Diesel DDC 93K218 '
+    level:'API CJ-4, CI-4 PLUS, CI-4, SM, ACEA E9-12 VDS-4, Mack EO-O Premium Plus, Renault RLD-3 , CES 20081, MB 228.31, Cat ECF-3, ECF-2, ECF-1-A, MAN 3575, Detroit Diesel DDC 93K218 ',
+    logoSAE:'false',
+    logoFor:'false',
+    logo0W:'false'
 }
-var khanc710w40 = {
+var khanc02 = {
     name: 'KHAN C-7 10W-40',
     style:'SYNTHETIC DIESEL',
-    level:'API CJ-4/SN | ACEA E9 MAN 3275 | Cummins CES20081 | Caterpillar ECF-3 | DDC 93K218 | Mack EO-N Premium Plus 03'
+    level:'API CJ-4/SN | ACEA E9 MAN 3275 | Cummins CES20081 | Caterpillar ECF-3 | DDC 93K218 | Mack EO-N Premium Plus 03',
+    logoSAE:'false',
+    logoFor:'false',
+    logo0W:'false'
 }
-var khanc715w40 = {
+var khanc03 = {
     name: 'KHAN C-7 15W-40',
     style:'SYNTHETIC DIESEL',
-    level:'API CJ-4, CI-4 PLUS, CI-4, SM, ACEA E9-12 VDS-4, Mack EO-O Premium Plus, Renault RLD-3 , CES 20081, MB 228.31, Cat ECF-3, ECF-2, ECF-1-A, MAN 3575, Detroit Diesel DDC 93K218'
+    level:'API CJ-4, CI-4 PLUS, CI-4, SM, ACEA E9-12 VDS-4, Mack EO-O Premium Plus, Renault RLD-3 , CES 20081, MB 228.31, Cat ECF-3, ECF-2, ECF-1-A, MAN 3575, Detroit Diesel DDC 93K218',
+    logoSAE:'false',
+    logoFor:'false',
+    logo0W:'false'
 }
+const khanCProduct = [khanc01,khanc02,khanc03]
+const khanC = jQuery('#khanC')
 //KHAN C-6 ...waiting......==================================================
 //KHAN C-5 ...waiting......==================================================
 //KHAN C-3 ...waiting......==================================================
 //KHAN BIKE 4T==================================================
-var khanbike4t10w40 = {
-    name: 'KHAN BIKE 4T 10W-40',
+var khanbike01 = {
+    name: '4T 10W-40',
     style:'FULLY SYNTHETIC BIKE',
-    level:'API SN JASO MB'
+    level:'API SN JASO MB',
+    logoSAE:'false',
+    logoFor:'false',
+    logo0W:'false'
 }
-var khanbike4t20w50 = {
-    name: 'KHAN BIKE 4T 20W-50',
+var khanbike02 = {
+    name: '4T 20W-50',
     style:'FULLY SYNTHETIC BIKE',
-    level:'API SL JASO MA2'
+    level:'API SL JASO MA2',
+    logoSAE:'false',
+    logoFor:'false',
+    logo0W:'false'
 }
-var khanbike4t10w30 = {
-    name: 'KHAN BIKE 4T 10W-30',
+var khanbike03 = {
+    name: '4T 10W-30',
     style:'FULLY SYNTHETIC BIKE',
-    level:'API SL/CF JASO MA'
+    level:'API SL/CF JASO MA',
+    logoSAE:'false',
+    logoFor:'false',
+    logo0W:'false'
 }
-var khanbike4t10w40 = {
-    name: 'KHAN BIKE 4T 10W-40',
+var khanbike04 = {
+    name: '4T 10W-40',
     style:'FULLY SYNTHETIC BIKE',
-    level:'API SL/CF JASO MA'
+    level:'API SL/CF JASO MA',
+    logoSAE:'false',
+    logoFor:'false',
+    logo0W:'false'
 }
-var khanbike4t20w40 = {
-    name: 'KHAN BIKE 4T 20W-40',
+var khanbike05 = {
+    name: '4T 20W-40',
     style:'FULLY SYNTHETIC BIKE',
-    level:'API SL/CF JASO MA'
+    level:'API SL/CF JASO MA',
+    logoSAE:'false',
+    logoFor:'false',
+    logo0W:'false'
 }
-var khanbike4t20w50 = {
-    name: 'KHAN BIKE 4T 20W-50',
+var khanbike06 = {
+    name: '4T 20W-50',
     style:'FULLY SYNTHETIC BIKE',
-    level:'API SL/CF JASO MA'
+    level:'API SL/CF JASO MA',
+    logoSAE:'false',
+    logoFor:'false',
+    logo0W:'false'
 }
+const khanBikeProduct = [khanbike01,khanbike02,khanbike03,khanbike04,khanbike05,khanbike06]
+const khanBike = jQuery('#khanBike')
 //KHAN MTF GL-5==================================================
-var khanmtfgl575w90 = {
+var khanmtf01 = {
     name: 'KHAN MTF GL-5 75W-90',
     style:'Manual Transmission Fluid',
-    level:'API GL-5'
+    level:'API GL-5 MIL-L-2105D',
+    logoSAE:'false',
+    logoFor:'false',
+    logo0W:'false'
 }
-var khanmtfgl580w90 = {
+var khanmtf02 = {
     name: 'KHAN MTF GL-5 80W-90',
     style:'Manual Transmission Fluid',
-    level:'API GL-5'
+    level:'API GL-5 MIL-L-2105D',
+    logoSAE:'false',
+    logoFor:'false',
+    logo0W:'false'
 }
-var khanmtfgl585w140 = {
+var khanmtf03 = {
     name: 'KHAN MTF GL-5 85W-140',
     style:'Manual Transmission Fluid',
-    level:'API GL-5'
+    level:'API GL-5 MIL-L-2105D',
+    logoSAE:'false',
+    logoFor:'false',
+    logo0W:'false'
 }
+const khanMtfProduct = [khanmtf01,khanmtf02,khanmtf03]
+const khanMtf = jQuery('#khanMtf')
 //KHAN ATF==================================================
-var khanatf3 = {
+var khanatf01 = {
     name: 'KHAN ATF 3',
     style:'SYNTHETIC ATF',
-    level:'Dexron IIIH/M | Allison C-4'
+    level:'Dexron IIIH/M | Allison C-4',
+    logoSAE:'false',
+    logoFor:'false',
+    logo0W:'false'
 }
-var khanatfmulti = {
+var khanatf02 = {
     name: 'KHAN ATF MULTI',
     style:'SYNTHETIC ATF',
-    level:'ATF MULTI'
+    level:'ATF MULTI',
+    logoSAE:'false',
+    logoFor:'false',
+    logo0W:'false'
 }
+const khanAtfProduct = [khanatf01,khanatf02]
+const khanAtf = jQuery('#khanAtf')
 //KHAN HYDRAULIC AW==================================================
-var khanaw32 = {
+var khanaw01 = {
     name: 'KHAN HYDRAULIC AW ',
     style:'물어봐야함',
-    level:'ISO VG 32'
+    level:'ISO VG 32',
+    logoSAE:'false',
+    logoFor:'false',
+    logo0W:'false'
 }
-var khanaw46 = {
+var khanaw02 = {
     name: 'KHAN HYDRAULIC AW ',
     style:'물어봐야함',
-    level:'ISO VG 46'
+    level:'ISO VG 46',
+    logoSAE:'false',
+    logoFor:'false',
+    logo0W:'false'
 }
-var khanaw68 = {
+var khanaw03 = {
     name: 'KHAN HYDRAULIC AW ',
     style:'물어봐야함',
-    level:'ISO VG 68'
+    level:'ISO VG 68',
+    logoSAE:'false',
+    logoFor:'false',
+    logo0W:'false'
 }
-//KHAN HYDRAULIC HV ...waiting...==================================================
+const khanAwProduct = [khanaw01,khanaw02,khanaw03]
+const khanAw = jQuery('#khanAw')
+//KHAN HYDRAULIC HV ...waiting...=======================================
+// #endregion product information=======================================
 
-//making object event=====================================================================
+//#region making object event===========================================
 function insertPdBtn(parent,product){//parent=jquery-selector, product=array
     for(i=0;i<product.length;i++){
         var name = product[i].name;
         var style = product[i].style;
         var level = product[i].level;
-        var li = $('<li data-name="'+name+'" data-style="'+style+'" data-level="'+level+'" class="series__product_text6listitem">'+name+'</li>');
+        var logo1 = product[i].logoSAE;
+        var logo2 = product[i].logoFor;
+        var logo3 = product[i].logo0W;
+        var li = jQuery('<li\
+                    data-style="'+style+'"\
+                    data-level="'+level+'"\
+                    data-logo1="'+logo1+'"\
+                    data-logo2="'+logo2+'"\
+                    data-logo3="'+logo3+'"\
+                    class="series__product_text6listitem">\
+                    '+name+'\
+                    </li>');
         parent.find('.series__product_text6list').append(li.clone())
     }
-    console.log('hi')
     parent.find('.series__product_text6listitem:first-child').addClass('clicked')
 }
 insertPdBtn(khanGoli,khanGoliProduct)
 insertPdBtn(khanPao,khanPaoProduct)
 insertPdBtn(khanG,khanGProduct)
+insertPdBtn(khanD,khanDProduct)
+insertPdBtn(khanC,khanCProduct)
+insertPdBtn(khanBike,khanBikeProduct)
+insertPdBtn(khanMtf,khanMtfProduct)
+insertPdBtn(khanAtf,khanAtfProduct)
+insertPdBtn(khanAw,khanAwProduct)
+//#endregion making object event===========================================
 
-
-//main event==============================================================================
-$('.series__name_listitem').click(function(){
-    idx = $(this).index()
-    $('li.series__product_listitem').removeClass('selected')
-    $('li.series__product_listitem').eq(idx).addClass('selected')
+//#region main event==============================================================================
+jQuery('.series__name_listitem').click(function(){//product menu fn
+    idx = jQuery(this).index()
+    jQuery('.series__name_listitem').removeClass('selected')
+    jQuery(this).addClass('selected')
+    jQuery('li.series__product_listitem').removeClass('selected')
+    jQuery('li.series__product_listitem').eq(idx).addClass('selected')
 })
-$('.series__product_text6listitem').click(function(){
-    var parent = $(this).closest('li.series__product_listitem');
-    var style = $(this).attr('data-style');
-    var level = $(this).attr('data-level');
+jQuery('.series__product_text6listitem').click(function(){//product model fn
+    var parent = jQuery(this).closest('li.series__product_listitem');
+    var style = jQuery(this).attr('data-style');
+    var level = jQuery(this).attr('data-level');
+    var logo1 = jQuery(this).attr('data-logo1')
+    var logo2 = jQuery(this).attr('data-logo2')
+    var logo3 = jQuery(this).attr('data-logo3')
     parent.find('.series__product_text4').text(level)
     parent.find('.series__product_text9').text(style)
-
-    $('.series__product_text6listitem').removeClass('clicked')
-    $(this).addClass('clicked')
+    parent.find('.series__product_text11 .series__product_logo').removeClass('true')
+    parent.find('.series__product_text11 .series__product_logo').removeClass('false')
+    parent.find('.series__product_text11 .series__product_logo1').addClass(logo1)
+    parent.find('.series__product_text11 .series__product_logo2').addClass(logo2)
+    parent.find('.series__product_text11 .series__product_logo3').addClass(logo3)
+    parent.find('.series__product_text6listitem').removeClass('clicked')
+    jQuery(this).addClass('clicked')
 })
-$('.series__product_text8 .volume_size').click(function(){
-    $('.series__product_text8 .volume_size').removeClass('clicked')
-    $(this).addClass('clicked')
+jQuery('.series__product_text8 .volume_size').click(function(){
+    var parent = jQuery(this).closest('li.series__product_listitem');
+    var dataType = jQuery(this).attr('data-type');
+    var dataSize = jQuery(this).attr('data-size');
+    parent.find('.series__product_text8 .volume_size').removeClass('clicked')
+    jQuery(this).addClass('clicked')
+    parent.find('.series__product_image_listitem').removeClass('selected')
+    parent.find('.series__product_image_listitem_'+dataType+dataSize).addClass('selected')
 })
+//#endregion main event==============================================================================
